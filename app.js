450ad7b946ea5d1fb3cf5a20775a1e3564cfd6a6
bethjaw <jaworskibeth@gmail.com>
2017-10-05 16:21:41 -0600

    added hbs and started working on connecting that and the data

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 var express = require('express')
 var app = express()
 var port = 3000
+const cupcakes = require('./data/cupcakes')
 
+app.set('view engine', 'hbs')
 
+app.get('/cupcakes/')
 
 app.get('/', (req, res) => {
-  res.send('yay!')
+  res.render('index', {
+    title: "Cupcake Reviews",
+    cupcakes: cupcakes
+  })
 })
+
 
 
 app.listen(port, () => {
---
+++ data/cupcakes.js
@@ -0,0 +1,29 @@
+const cupcakes = [
+  {
+    id: 0,
+    flavor: 'Chocolate with carmel sea salt',
+    description: 'Chocolate cake with caramel sea salt buttercream and caramel drizzle',
+    baker: 'Michelle',
+    ratings: [4, 5, 4, 5],
+    // averageRating:
+  },
+  {
+    id: 1,
+    flavor: 'Chocolate Chocolate',
+    description: 'Chocolate cake with chocolate buttercream and cocoa shavings',
+    baker: 'Beth',
+    ratings: [4, 5, 3, 4],
+    // averageRating:
+  },
+  {
+    id: 2,
+    flavor: 'Vanilla Strawberry Dream',
+    description: 'Vanilla bean cake with strawberry buttercream and strawberry slivers',
+    baker: 'Megan',
+    ratings: [3, 4, 4, 4],
+    // averageRating:
+  }
+]
+
+
+module.exports = cupcakes
